aaa8b8f5090b86a89485cadd351d8b5f8fd31333
jstmax! <87650746+jstmaxlol@users.noreply.github.com>
2024-12-11 20:14:08 +0100

    > add fish_right_prompt and update fish_prompt

---
+++ jstmax_fish_prompts.fish
@@ -0,0 +1,21 @@
+# fish_prompt
+function fish_prompt
+    if test "$PWD" = "/home/maxlol"
+      echo -e (set_color green)$USER(set_color white)"@"(set_color green)"~"\n(set_color white)":: "
+    else
+      echo -e (set_color green)$USER(set_color white)"@"(set_color green)$PWD\n(set_color white)":: "
+    end
+end
+
+# fish_right_prompt
+function fish_right_prompt
+        set last_status $status
+
+        # check for which color to use
+        if test $last_status -eq 0
+                set color green
+        else
+                set color red
+        end
+        echo -e (set_color $color)"[$last_status]"
+end
