9db08c9145499468f63aa3fb5301fc8fddbeb38b
jstmax! <87650746+jstmaxlol@users.noreply.github.com>
2025-01-28 18:42:48 +0100

    > adding logs

--- jmPIUS.sh
+++ jmPIUS.sh
@@ -23,21 +23,26 @@
 
 # create working directory - 
 sudo mkdir ~/.jmtemp/
+echo -e "\n\n\n> creating working directory at ~/.jmtemp ..\n\n"
 
 ## move to working dir
 cd ~/.jmtemp/
+echo -e "\n\n\n> cding (moving) to working dir ..\n\n"
 
 ### install pacman packages
 sudo wget -P ~/.jmtemp/ https://github.com/jstmaxlol/jstmaxlol/raw/refs/heads/main/towget/jm_pkgs_list.txt
 grep -v '^#' ~/.jmtemp/jm_pkgs_list.txt | tr -d '\r' | xargs sudo pacman -Syu --noconfirm
+echo -e "\n\n\n> installing pacman packages ..\n\n"
 
 ####
 ##### make fish the default shell
 echo /usr/local/bin/fish | sudo tee -a /etc/shells && chsh -s /usr/local/bin/fish
+echo -e "\n\n\n> setting fish as default shell ..\n\n"
 
 ###### do fish prompt stuff
 sudo wget -P ~/.config/fish/functions/ https://github.com/jstmaxlol/jstmaxlol/raw/refs/heads/main/towget/fish_prompt.fish
 sudo wget -P ~/.config/fish/functions/ https://github.com/jstmaxlol/jstmaxlol/raw/refs/heads/main/towget/fish_right_prompt.fish
+echo -e "\n\n\n> setting fish prompts ..\n\n"
 
 ####### install some of my scheiBe from github
 ####### xsc
@@ -45,12 +50,16 @@ sudo wget -P ~/.jmtemp/ https://github.com/jstmaxlol/XSC/raw/main/xsc.cpp
 sudo g++ -o xsc ~/.jmtemp/xsc.cpp
 sudo cp ~/.jmtemp/xsc /usr/bin/xsc
 sudo chmod +x /usr/bin/xsc
+echo -e "\n\n\n> installing xsc ..\n\n"
 ########
 ######### add custom ~/.vimrc
 sudo wget -P ~/ https://github.com/jstmaxlol/jstmaxlol/raw/main/.vimrc
+echo -e "\n\n\n> adding custom ~/.vimrc ..\n\n"
 
 ########## install yay
+echo -e "\n\n\n> installing yay - start..\n\n"
 cd ~ && sudo pacman -S --needed --noconfirm git base-devel && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
+echo -e "\n\n\n> installing yay - end.\n\n"
 
 ########### final bye bye message and notif :3
 echo "[n!a]     dO nOT fORGET tO rEBOOT yOUR sYSTEM!!!!!!!!!!!" ###### 1
