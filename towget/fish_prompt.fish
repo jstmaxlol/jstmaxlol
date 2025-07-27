94456b7b7066a96597357fa36400e205e15eafa0
jStmaX! <87650746+jstmaxlol@users.noreply.github.com>
2025-07-27 19:41:48 +0200

    # delete pius old shit

--- towget/fish_prompt.fish
+++
@@ -1,7 +0,0 @@
-function fish_prompt
-    if test "$PWD" = "/home/$USER"
-      echo -e (set_color green)$USER(set_color white)"@"(set_color green)"~"\n(set_color white)":: "
-    else
-      echo -e (set_color green)$USER(set_color white)"@"(set_color green)$PWD\n(set_color white)":: "
-    end
-end
