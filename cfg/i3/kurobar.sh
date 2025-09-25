339198d3473efee66afb89b9a3b80ee35a2ac766
jStmaX! <87650746+jstmaxlol@users.noreply.github.com>
2025-09-25 23:44:18 +0200

    update kuroba

--- cfg/i3/kurobar.sh
+++ cfg/i3/kurobar.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 while true; do
+        # --- volume percentage ---
+        volume=$(pamixer --get-volume)
+
     # --- battery percentage ---
     if [ -d /sys/class/power_supply/BAT0 ]; then
         bat=$(cat /sys/class/power_supply/BAT0/capacity)
@@ -20,7 +23,7 @@ while true; do
     dt=$(date '+%Y-%m-%d %H:%M:%S')
 
     # --- print bar line ---
-    echo "$bat% | $mem_used/$mem_total GB | $bright | $dt"
+    echo "V.$volume% | B.$bat% | Mu.$mem_used/Mt.$mem_total GB | L.$bright | $dt"
 
-    sleep 0.1
+    sleep 0.01
 done
