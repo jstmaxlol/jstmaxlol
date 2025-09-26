596882c8345a0d9ca70b57f36444905e714606f6
jStmaX! <87650746+jstmaxlol@users.noreply.github.com>
2025-09-26 21:46:28 +0200

    ##

--- cfg/config.fish
+++ cfg/config.fish
@@ -16,6 +16,9 @@ if status is-interactive
         alias nvimrc "nvim ~/config/nvim/init.vim"
         alias ":q" exit
         alias "replasma" "killall plasmashell && kstart5 plasmashell"
+        alias i3rc "vim ~/.config/i3/config"
+        alias kurobarc "vim ~/.config/i3/kurobar.sh"
+        alias jmdmenurc "sudo vim /usr/bin/jmdmenu"
         # locale fucked - fix:
         #set -x LANG en_US.UTF-08
         #set -x LC_ALL en_US.UTF-08
