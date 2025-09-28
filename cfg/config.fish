af672dcab28a0d66142f16ae53cbe18091c5f8e3
jStmaX! <87650746+jstmaxlol@users.noreply.github.com>
2025-09-29 00:26:07 +0200

    update fish cfg

--- cfg/config.fish
+++ cfg/config.fish
@@ -1,7 +1,7 @@
 # ~/.config/fish/config.fish
 if status is-interactive
         # other env vars
-        set -x EDITOR vim
+        set -x EDITOR nvim
         #set -x WINEPREFIX ~/winestuff/ps2018
         set -x WINEARCH win64
         # aliases because i am a [NOTHING BAD I SWEAR]
@@ -10,15 +10,16 @@ if status is-interactive
         alias ff fastfetch
         alias nf nfetch
         alias fishsrc "source ~/.config/fish/config.fish"
-        alias vimrc "vim ~/.vimrc"
-        alias fishrc "vim ~/.config/fish/config.fish"
+        alias vimrc "nvim ~/.vimrc"
+        alias fishrc "nvim ~/.config/fish/config.fish"
         alias neofishrc "nvim ~/.config/fish/config.fish"
         alias nvimrc "nvim ~/.config/nvim/init.vim"
         alias ":q" exit
         alias replasma "killall plasmashell && kstart5 plasmashell"
-        alias i3rc "vim ~/.config/i3/config"
-        alias kurobarc "vim ~/.config/i3/kurobar.sh"
-        alias jmdmenurc "sudo vim /usr/bin/jmdmenu"
+        alias i3rc "nvim ~/.config/i3/config"
+        alias kurobarc "nvim ~/.config/i3/kurobar.sh"
+        alias jmdmenurc "sudo -E nvim /usr/bin/jmdmenu"
+        alias ghosttyrc "nvim ~/.config/ghostty/config"
         # locale fucked - fix:
         #set -x LANG en_US.UTF-08
         #set -x LC_ALL en_US.UTF-08
