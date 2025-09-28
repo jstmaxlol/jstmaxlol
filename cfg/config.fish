aeefc8a591bf8f369acb8c79759d3bec27ca8b97
jStmaX! <87650746+jstmaxlol@users.noreply.github.com>
2025-09-28 14:31:22 +0200

    OH YEAH SHIT NEOVIM

--- cfg/config.fish
+++ cfg/config.fish
@@ -5,17 +5,17 @@ if status is-interactive
         #set -x WINEPREFIX ~/winestuff/ps2018
         set -x WINEARCH win64
         # aliases because i am a [NOTHING BAD I SWEAR]
-        alias nv nvim
-        alias v vim
+        alias v nvim
+        #alias v vim
         alias ff fastfetch
         alias nf nfetch
         alias fishsrc "source ~/.config/fish/config.fish"
         alias vimrc "vim ~/.vimrc"
         alias fishrc "vim ~/.config/fish/config.fish"
         alias neofishrc "nvim ~/.config/fish/config.fish"
-        alias nvimrc "nvim ~/config/nvim/init.vim"
+        alias nvimrc "nvim ~/.config/nvim/init.vim"
         alias ":q" exit
-        alias "replasma" "killall plasmashell && kstart5 plasmashell"
+        alias replasma "killall plasmashell && kstart5 plasmashell"
         alias i3rc "vim ~/.config/i3/config"
         alias kurobarc "vim ~/.config/i3/kurobar.sh"
         alias jmdmenurc "sudo vim /usr/bin/jmdmenu"
