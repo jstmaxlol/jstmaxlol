4da38055970bc983b4a2d1a7ea04795cdde15bd1
jstmax! <87650746+jstmaxlol@users.noreply.github.com>
2025-10-26 00:26:29 +0200

    updated cfg/ - testing commit signature from git

--- cfg/config.fish
+++ cfg/config.fish
@@ -1,26 +1,34 @@
 # ~/.config/fish/config.fish
 if status is-interactive
-        # other env vars
-        set -x EDITOR nvim
-        #set -x WINEPREFIX ~/winestuff/ps2018
-        set -x WINEARCH win64
-        # aliases because i am a [NOTHING BAD I SWEAR]
-        alias v nvim
-        #alias v vim
-        alias ff fastfetch
-        alias nf nfetch
-        alias fishsrc "source ~/.config/fish/config.fish"
-        alias vimrc "nvim ~/.vimrc"
-        alias fishrc "nvim ~/.config/fish/config.fish"
-        alias neofishrc "nvim ~/.config/fish/config.fish"
-        alias nvimrc "nvim ~/.config/nvim/init.vim"
-        alias ":q" exit
-        alias replasma "killall plasmashell && kstart5 plasmashell"
-        alias i3rc "nvim ~/.config/i3/config"
-        alias kurobarc "nvim ~/.config/i3/kurobar.sh"
-        alias jmdmenurc "sudo -E nvim /usr/bin/jmdmenu"
-        alias ghosttyrc "nvim ~/.config/ghostty/config"
-        # locale fucked - fix:
-        #set -x LANG en_US.UTF-08
-        #set -x LC_ALL en_US.UTF-08
+
+	# ENVIRONMENT VARIABLES
+	set -x EDITOR nvim
+	set -x TERMINAL alacritty
+	set -x VIMRUNTIME "/usr/share/nvim/runtime"
+    #set -x WINEPREFIX ~/winestuff/ps2018
+	set -x WINEARCH win64
+
+	# ALIASES
+	alias v nvim
+	alias ff fastfetch
+	alias nf nfetch
+	alias src "source ~/.config/fish/config.fish"
+	alias vimrc "nvim ~/.vimrc"
+	alias fishrc "nvim ~/.config/fish/config.fish"
+	alias neofishrc "nvim ~/.config/fish/config.fish"
+	alias nvimrc "nvim ~/.config/nvim/init.vim"
+	alias ":q" exit
+	alias replasma "killall plasmashell && kstart5 plasmashell"
+	alias i3rc "nvim ~/.config/i3/config"
+	alias kurobarc "nvim ~/.config/i3/kurobar.sh"
+	alias jmdmenurc "sudo -E nvim /usr/bin/jmdmenu"
+	alias ghosttyrc "nvim ~/.config/ghostty/config"
+	alias alattyrc "nvim ~/.config/alacritty/alacritty.toml"
+	alias suv "sudo -E nvim"
+	alias ffrc "nvim ~/.config/fastfetch/config.jsonc"
+	alias cfr "java -jar /usr/bin/cfr.jar"
+	alias py "python"
+	alias drumz "drumseq ch ch h ch s ch h s -x -bpm=360"
+    alias dashbd-clock "tty-clock -s -c -C 1"
+    alias trexa "exa --tree --level=2 --icons"
 end
