d8a739b332a2760e2866d0563585def80b85dc07
czjstmax <jstmaxlol@disroot.org>
2026-03-24 02:04:42 +0100

    made it less shitttt
    Signed-off-by: czjstmax <jstmaxlol@disroot.org>

--- clear.c
+++ clear.c
@@ -1,17 +1,20 @@
 #include <stdio.h>
-#include <clear.h>
 
 int main(int argc, char **argv)
 {
     if (argc >= 2) {
         char *grammar = (argc > 2)? "arguments" : "argument";
+
         printf("%d %s found. %s won't be checked.\n", argc-1, grammar, grammar);
+
         for (int i = 1; i < argc; i++) {
             printf("argument %d = %s\n", i, argv[i]);
         }
+
         printf("aborting insane clear operation.\n");
         return 1;
     }
-    printf("%s", ANSI_CLEAR_SCREEN_AND_SCROLLBACK);
+
+    puts("\033[2J\033[3J\033[H");
     return 0;
 }
