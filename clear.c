ca28a6b97267394311e146264d8040af343ca107
czjstmax <jstmaxlol@disroot.org>
2026-04-13 14:48:08 +0200

    removed `clear.c` (commit desc. for details)
    removed `clear.c`, reason: moved to [jstmaxlol/clear](https://github.com/jstmaxlol/clear)
    
    Signed-off-by: czjstmax <jstmaxlol@disroot.org>

--- clear.c
+++ clear.c
@@ -7,9 +7,8 @@ int main(int argc, char **argv)
 
         printf("%d %s found. %s won't be checked.\n", argc-1, grammar, grammar);
 
-        for (int i = 1; i < argc; i++) {
+        for (int i = 1; i < argc; i++)
             printf("argument %d = %s\n", i, argv[i]);
-        }
 
         printf("aborting insane clear operation.\n");
         return 1;
